09dcb07f374d2e2cfc7d3ae753886951116c0dda
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-13 15:29:32 -0500

    add tap and capture object name

--- ramp-up/Controller/RampPickerVC.swift
+++ ramp-up/Controller/RampPickerVC.swift
@@ -14,6 +14,8 @@ class RampPickerVC: UIViewController {
     let SCN_ASSETS = "art.scnassets/"
     var sceneView: SCNView!
     var size: CGSize!
+    weak var rampPlacerVC: RampPlacerVC!
+    
     
     init(size: CGSize) {
         super.init(nibName: nil, bundle: nil)
@@ -40,12 +42,17 @@ class RampPickerVC: UIViewController {
         camera.usesOrthographicProjection = true
         scene.rootNode.camera = camera
         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        sceneView.addGestureRecognizer(tap)
+        
+        let rotate = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(0.01 * Double.pi), z: 0, duration: 0.1))
+        
         var obj: SCNScene?
         var node: SCNNode?
         
         obj = SCNScene(named: "\(SCN_ASSETS)pipe.dae")
         node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
-
+        node?.runAction(rotate)
         node?.scale = SCNVector3Make(0.0015, 0.0015, 0.0015)
         node?.position = SCNVector3Make(0, -0.4, 0)
         scene.rootNode.addChildNode(node!)
@@ -54,19 +61,29 @@ class RampPickerVC: UIViewController {
 
         obj = SCNScene(named: "\(SCN_ASSETS)pyramid.dae")
         node = obj?.rootNode.childNode(withName: "pyramid", recursively: true)!
+        node?.runAction(rotate)
         node?.scale = SCNVector3Make(0.0045, 0.0045, 0.0045)
         node?.position = SCNVector3Make(0, -1.25, 0)
         scene.rootNode.addChildNode(node!)
         
         obj = SCNScene(named: "\(SCN_ASSETS)quarter.dae")
         node = obj?.rootNode.childNode(withName: "quarter", recursively: true)!
+        node?.runAction(rotate)
         node?.scale = SCNVector3Make(0.0045, 0.0045, 0.0045)
         node?.position = SCNVector3Make(0, -2.5, 0)
         scene.rootNode.addChildNode(node!)
         
     }
 
-    
+    @objc func handleTap(_ gestureRecognizer: UIGestureRecognizer) {
+        let p = gestureRecognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(p, options: [:])
+        if hitResults.count > 0 {
+            let node = hitResults[0].node
+            print(node.name!)
+            rampPlacerVC.onRampSelected(node.name!)
+        }
+    }
     
 
     
