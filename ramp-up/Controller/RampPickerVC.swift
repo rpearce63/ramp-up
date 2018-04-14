b572856a04d20d782d7ab83bd58f7e728a33091b
Rick Pearce <rpearce63@yahoo.com>
2018-04-14 07:50:53 -0500

    add obj to scene and move around or delete

--- ramp-up/Controller/RampPickerVC.swift
+++ ramp-up/Controller/RampPickerVC.swift
@@ -34,6 +34,8 @@ class RampPickerVC: UIViewController {
         view.insertSubview(sceneView, at: 0)
         
         preferredContentSize = size
+        view.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        view.layer.borderWidth = 3.0
         
         let scene = SCNScene(named: "\(SCN_ASSETS)ramps.scn")!
         sceneView.scene = scene
@@ -66,6 +68,7 @@ class RampPickerVC: UIViewController {
             let node = hitResults[0].node
             print(node.name!)
             rampPlacerVC.onRampSelected(node.name!)
+            dismiss(animated: true, completion: nil)
         }
     }
     
